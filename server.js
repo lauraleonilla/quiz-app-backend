ba54e00b1f0c613ec7d613c1eaebb843be64aa00
Leonilla <leonillakoskivaara@Leonillas-MacBook-Pro-2.local>
2019-12-22 19:04:44 +0100

    Added morgan

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const express = require('express')
 const http = require('http')
+const morgan = require('morgan')
 const bodyParser = require('body-parser')
 const app = express()
 const config = require('./config')
+
+app.use(bodyParser.json())
+app.use(
+  morgan(':method :url :status :response-time ms - :res[content-length] :body')
+)
+morgan.token('body', (req, res) => JSON.stringify(req.body))
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
