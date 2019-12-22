275fd19bb7a489a65dd01ccf5f0238b10861d7e2
Leonilla <leonillakoskivaara@Leonillas-MacBook-Pro-2.local>
2019-12-22 22:58:24 +0100

    Added login and user routes

---
+++ routes/user.js
@@ -0,0 +1,35 @@
+const bcrypt = require('bcrypt')
+const usersRouter = require('express').Router()
+const User = require('../models/user')
+
+usersRouter.get('/', async (req, res) => {
+  const users = await User.find({})
+  res.json(users.map(u => u.toJSON()))
+})
+
+usersRouter.post('/', async (req, res, next) => {
+  try {
+    const body = req.body
+    if (!body.password || body.password.length < 3) {
+      return res.status(400).json({
+        error: 'Give a password with at least three characters'
+      })
+    }
+    const saltRounds = 10
+    const passwordHash = await bcrypt.hash(body.password, saltRounds)
+
+    const user = new User({
+      username: body.username,
+      name: body.name,
+      passwordHash,
+    })
+
+    const savedUser = await user.save()
+
+    res.json(savedUser)
+  } catch (exception) {
+    next(exception)
+  }
+})
+
+module.exports = usersRouter
--- timeKeeping.md
+++ timeKeeping.md
@@ -5,7 +5,7 @@ Below the dates and number of hours worked.
 | 24.11  | 1      | Set up express server. Created a React frontEnd. | 
 | 1.12   | 1      | Added linter, started with frontend jsx. | 
 | 21.12  | 4      | Started adding routes, implemented redux and added elements to the frontend. | 
-| 22.12  | ?      | IMPLEMENT DB | 
-| ?????  | ?      | IMPLEMENT LOGIN | 
+| 22.12  | 4      | Added mongo db and routes for user and login. | 
+| 23.12  | ?      | IMPLEMENT LOGIN FRONTEND | 
 | ?????  | ?      | IMPLEMENT OAUTH | 
-| Total hours | 6 |              | 
+| Total hours | 10 |              | 
