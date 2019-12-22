a94085f85f7afe9fa5f5331acd931baab3ff4dd8
Leonilla <leonillakoskivaara@Leonillas-MacBook-Pro-2.local>
2019-12-22 20:03:07 +0100

    Added connection to mongodb

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const http = require('http')
 const morgan = require('morgan')
 const bodyParser = require('body-parser')
 const app = express()
 const config = require('./config')
+
+console.log('connecting to', config.MONGODB_URI)
+
+mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('Error connecting to MongoDB', error.message)
+  })
 
 app.use(bodyParser.json())
 app.use(
