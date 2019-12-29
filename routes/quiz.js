f650dc372dbcddbc27f0e7c3a342ac3087577231
Leonilla <leonillakoskivaara@Leonillas-MacBook-Pro-2.local>
2019-12-29 21:43:22 +0200

    Updating score with put request

--- routes/quiz.js
+++ routes/quiz.js
@@ -11,12 +11,32 @@ quizRouter.post('/score', async (req, res, next) => {
     const user = await User.findById(userId)
     const newScore = new Score({
       quiz,
-      score
+      score,
+      user
     })
     const savedScore = await newScore.save()
     user.scores = user.scores.concat(savedScore._id)
     await user.save()
     res.json(savedScore.toJSON())
+  } catch (exception) {
+    next(exception)
+  }
+})
+
+quizRouter.put('/score', async (req, res, next) => {
+  const quiz = req.body.quiz
+  const user = req.body.userId
+  const score = req.body.score
+  const filter = { user, quiz }
+  try {
+    const updatedScore = await Score.findOneAndUpdate(
+      filter,
+      { score: score },
+      {
+        new: true
+      }
+    )
+    res.json(updatedScore.toJSON())
   } catch (exception) {
     next(exception)
   }
