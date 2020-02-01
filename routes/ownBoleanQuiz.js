d7a8a855a4b09602a92611bb9b3acc69be60159b
Leonilla <leonillakoskivaara@Leonillas-MacBook-Pro-2.local>
2020-02-01 11:01:44 +0100

    Edited timekeeper

--- routes/ownBoleanQuiz.js
+++ routes/ownBoleanQuiz.js
@@ -13,16 +13,19 @@ ownBooleanQuiz.post('/', async (req, res, next) => {
       return res.status(401).json({ error: 'Token missing or invalid' })
     }
     const user = await User.findById(decodedToken.id)
-    console.log('Here we are!', questions, user)
     const newQuiz = new Quiz({
       quizTitle: quizTitle,
       questions: questions,
       user: user._id
     })
-    const savedQuiz = await newQuiz.save()
-    user.booleanQuizzes = user.booleanQuizzes.concat(savedQuiz._id)
-    await user.save()
-    res.json(savedQuiz.toJSON())
+    try {
+      const savedQuiz = await newQuiz.save()
+      user.booleanQuizzes = user.booleanQuizzes.concat(savedQuiz._id)
+      await user.save()
+      res.json(savedQuiz.toJSON())
+    } catch (error) {
+      return res.status(400).json({ error: error })
+    }
   } catch (exception) {
     next(exception)
   }
